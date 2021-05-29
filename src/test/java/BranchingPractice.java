012ec3854147eb8d73a5ff532c573de9a8a1a1f4
Seda-Gedik <sedayesilyurt58@gmail.com>
2021-05-29 18:05:58 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,6 +2,13 @@ public class BranchingPractice {
     public static void main(String[] args) {
 
 
+        // create a new project
+        // enable version control
+        // added .gitignore with .idea, target folder, and .iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
+        
+
 
     }
 }
